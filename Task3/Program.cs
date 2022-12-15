b4d1f4f7332e94369eecc95af717c5f816e958bc
Elena <lenka.penka123412@gmail,com>
2022-12-15 14:30:24 +0300

    Сортировка массива

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Сортировка массива
+
+Console.Clear();
+
+int[] arr = {1, 3, 9, 8, 7, 2, 5, 4, 5, 8, 6, 9, 7, 3};
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPositoin = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPositoin]) minPositoin = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPositoin];
+        array[minPositoin] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
