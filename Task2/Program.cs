a74cef91191c1a50baf869ff10e2e76191534243
Elena <lenka.penka123412@gmail,com>
2022-12-15 14:01:37 +0300

    Замена символов

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Замена символов
+string text = "В нору забилась старенькая осень, "
+            + "Понабросала листьев, как лисят. "
+            + "Друзей уносит. И врагов уносит. "
+            + "Лет в сорок восемь или пятьдесят.";
+
+string RepLace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = RepLace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = RepLace(text, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = RepLace(text, 'с', 'С');
+Console.WriteLine(newText);
